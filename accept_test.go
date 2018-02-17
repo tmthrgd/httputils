2983d2a618d8dcb8ae4b754d792a90d67950f536
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-02-17 12:59:02 +1030

    Add HTTP content negotiation function

---
+++ accept_test.go
@@ -0,0 +1,194 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package httputils
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNegotiate(t *testing.T) {
+	for i, tt := range []struct {
+		header   []string
+		offers   []string
+		expected string
+	}{
+		// Accept
+		{
+			[]string{"text/html, application/xhtml+xml, application/xml;q=0.9, */*; q=0.8"},
+			[]string{"application/example", "application/xml", "text/html", "application/xhtml+xml"},
+			"text/html",
+		},
+		{
+			[]string{"text/html, application/xhtml+xml, application/xml;q=0.9, */*; q=0.8 "},
+			[]string{"application/example", "application/xml", "application/xhtml+xml", "text/html"},
+			"application/xhtml+xml",
+		},
+		{
+			[]string{"TeXt/HtMl, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8"},
+			[]string{"application/example", "application/xml", "text/html", "application/xhtml+xml"},
+			"text/html",
+		},
+		{
+			[]string{"text/html, application/xhtml+xml, application/xml; q=0.9, */*;q=0.8"},
+			[]string{"application/example", "*/*"},
+			"*/*",
+		},
+		{
+			[]string{"text/html, application/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"},
+			[]string{"application/example"},
+			"",
+		},
+		{
+			[]string{"text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8"},
+			nil,
+			"",
+		},
+		{
+			[]string{"TeXt/HtMl,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"},
+			[]string{"application/example", "application/xml", "TEXT/HTML", "application/xhtml+xml"},
+			"TEXT/HTML",
+		},
+		{
+			[]string{"text/html", "application/xhtml+xml", "application/xml;q=0.9", "*/*;q=0.8"},
+			[]string{"application/example", "application/xml", "text/html", "application/xhtml+xml"},
+			"text/html",
+		},
+		{
+			[]string{"text/html", "application/xhtml+xml", "application/xml;q=0.9", "*/*;q=0.8"},
+			[]string{"application/example", "application/xml", "application/xhtml+xml"},
+			"application/xhtml+xml",
+		},
+		{
+			[]string{"text/html", "application/xhtml+xml", "application/xml;   q=0.9  ", " */*;  q=0.8 "},
+			[]string{"application/example", "application/xml"},
+			"application/xml",
+		},
+		// TODO: fix leading whitespace
+		//{
+		//	[]string{" text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8"},
+		//	[]string{"application/example", "application/xml", "text/html", "application/xhtml+xml"},
+		//	"text/html",
+		//},
+		{
+			nil,
+			[]string{"application/example"},
+			"",
+		},
+
+		// Accept-Charset
+		{
+			[]string{"utf-8, iso-8859-1;q=0.5"},
+			[]string{"invalid", "iso-8859-1", "utf-8"},
+			"utf-8",
+		},
+		{
+			[]string{"utf-8, iso-8859-1;q=0.5"},
+			[]string{"invalid", "iso-8859-1"},
+			"iso-8859-1",
+		},
+		{
+			[]string{"utf-8", "iso-8859-1;q=0.5"},
+			[]string{"invalid", "iso-8859-1"},
+			"iso-8859-1",
+		},
+		{
+			[]string{"utf-8, iso-8859-1;q=0.5"},
+			[]string{"invalid"},
+			"",
+		},
+		{
+			[]string{"utf-8, iso-8859-1;q=0.5"},
+			nil,
+			"",
+		},
+		{
+			nil,
+			[]string{"invalid"},
+			"",
+		},
+
+		// Accept-Encoding
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5"},
+			[]string{"invalid", "gzip", "deflate"},
+			"gzip",
+		},
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5"},
+			[]string{"invalid", "*", "deflate"},
+			"deflate",
+		},
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5"},
+			[]string{"invalid", "*"},
+			"*",
+		},
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5, identity;q=0.1"},
+			[]string{"invalid", "identity"},
+			"identity",
+		},
+		{
+			[]string{"deflate;q=0.7, gzip;q=0.9, *;q=0.5, identity;q=0.1", "br"},
+			[]string{"invalid", "gzip", "identity", "br"},
+			"br",
+		},
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5"},
+			[]string{"invalid"},
+			"",
+		},
+		{
+			[]string{"deflate, gzip;q=1.0, *;q=0.5"},
+			nil,
+			"",
+		},
+		{
+			nil,
+			[]string{"invalid"},
+			"",
+		},
+
+		// Accept-Language
+		{
+			[]string{"fr-CH, fr;q=0.9, en;q=0.8", "de;q=0.7, *;q=0.5"},
+			[]string{"invalid", "en", "de", "fr"},
+			"fr",
+		},
+		{
+			[]string{"fr-CH, fr;q=0.9, en;q=0.8", "de;q=0.7, *;q=0.5"},
+			[]string{"invalid", "en", "de", "fr-CH"},
+			"fr-CH",
+		},
+		{
+			[]string{"fr-CH, fr;q=0.9, en;q=0.8", "de;q=0.7, *;q=0.5"},
+			[]string{"invalid", "de", "en-US", "en-GB", "en"},
+			"en",
+		},
+		{
+			[]string{"fr-CH, fr;q=0.9, en;q=0.8", "de;q=0.7, *;q=0.5"},
+			[]string{"invalid"},
+			"",
+		},
+		{
+			[]string{"fr-CH, fr;q=0.9, en;q=0.8", "de;q=0.7, *;q=0.5"},
+			nil,
+			"",
+		},
+		{
+			nil,
+			[]string{"invalid"},
+			"",
+		},
+	} {
+		assert.Equal(t, tt.expected, Negotiate(http.Header{
+			"X-Accept": tt.header,
+		}, "X-Accept", tt.offers...), "#%d %q", i, tt)
+	}
+}
---
+++ parse-accept_test.go
@@ -0,0 +1,53 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd.
+
+package httputils
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+var parseAcceptTests = []struct {
+	s        string
+	expected []acceptSpec
+}{
+	{"text/html", []acceptSpec{{"text/html", 1}}},
+	{"text/html; q=0", []acceptSpec{{"text/html", 0}}},
+	{"text/html; q=0.0", []acceptSpec{{"text/html", 0}}},
+	{"text/html; q=1", []acceptSpec{{"text/html", 1}}},
+	{"text/html; q=1.0", []acceptSpec{{"text/html", 1}}},
+	{"text/html; q=0.1", []acceptSpec{{"text/html", 0.1}}},
+	{"text/html;q=0.1", []acceptSpec{{"text/html", 0.1}}},
+	{"text/html, text/plain", []acceptSpec{{"text/html", 1}, {"text/plain", 1}}},
+	{"text/html; q=0.1, text/plain", []acceptSpec{{"text/html", 0.1}, {"text/plain", 1}}},
+	{"iso-8859-5, unicode-1-1;q=0.8,iso-8859-1", []acceptSpec{{"iso-8859-5", 1}, {"unicode-1-1", 0.8}, {"iso-8859-1", 1}}},
+	{"iso-8859-1", []acceptSpec{{"iso-8859-1", 1}}},
+	{"*", []acceptSpec{{"*", 1}}},
+	{"da, en-gb;q=0.8, en;q=0.7", []acceptSpec{{"da", 1}, {"en-gb", 0.8}, {"en", 0.7}}},
+	{"da, q, en-gb;q=0.8", []acceptSpec{{"da", 1}, {"q", 1}, {"en-gb", 0.8}}},
+	{"image/png, image/*;q=0.5", []acceptSpec{{"image/png", 1}, {"image/*", 0.5}}},
+	{"text/html; Q=1", []acceptSpec{{"text/html", 1}}},
+	{"text/html; q=0.123", []acceptSpec{{"text/html", 0.123}}},
+
+	// bad cases
+	{"value1; q=0.1.2", []acceptSpec{{"value1", 0.1}}},
+	{"da, en-gb;q=foo", []acceptSpec{{"da", 1}}},
+
+	// failing cases
+	{"text/html; q=0.1234", nil},
+}
+
+func TestParseAccept(t *testing.T) {
+	for _, tt := range parseAcceptTests {
+		header := http.Header{"Accept": {tt.s}}
+		actual := parseAccept(header, "Accept")
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("ParseAccept(h, %q)=%v, want %v", tt.s, actual, tt.expected)
+		}
+	}
+}
